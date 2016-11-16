b5d76cae9e906aff9e247d3175e880e197d626c0
sdelamo <sergio.delamo@softamo.com>
2016-11-16 11:20:33 +0100

    Remove install task

--- travis-build.sh
+++ travis-build.sh
@@ -5,7 +5,7 @@ echo "TRAVIS_BRANCH       : $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST : $TRAVIS_PULL_REQUEST"
 echo "Publishing archives for branch $TRAVIS_BRANCH"
 
-./gradlew clean check install --stacktrace
+./gradlew clean check --stacktrace
 
 EXIT_STATUS=0
 echo "Publishing archives for branch $TRAVIS_BRANCH"
