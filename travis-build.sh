cab2ab54a218628d402b6f6470013884d16bd7a1
sdelamo <sergio.delamo@softamo.com>
2016-11-30 17:02:24 +0100

    Add publishing gradle files

--- travis-build.sh
+++ travis-build.sh
@@ -5,17 +5,14 @@ echo "TRAVIS_BRANCH       : $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST : $TRAVIS_PULL_REQUEST"
 echo "Publishing archives for branch $TRAVIS_BRANCH"
 
-./gradlew clean check publishToMavenLocal --stacktrace
+./gradlew clean check--stacktrace
 
 EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
+
 if [[ -n ${TRAVIS_TAG} ]] || [[ ${TRAVIS_BRANCH} == 'master' && ${TRAVIS_PULL_REQUEST} == 'false' ]]; then
   if [[ -n ${TRAVIS_TAG} ]]; then
     echo "Pushing build to Bintray"
-    ./gradlew bintrayUpload -PpluginVersion=${TRAVIS_TAG}  || EXIT_STATUS=$?
-  else
-    echo "Publishing snapshot to OJO"
-    ./gradlew artifactoryPublish || EXIT_STATUS=$?
+    ./gradlew release -PpluginVersion=${TRAVIS_TAG}  || EXIT_STATUS=$?
   fi
 fi
 exit ${EXIT_STATUS}
