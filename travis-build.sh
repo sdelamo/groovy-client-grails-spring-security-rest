70f66dddad575729e0a2a8d8b3cb8ae57a2a280d
sdelamo <sergio.delamo@softamo.com>
2016-12-01 07:12:57 +0100

    Add missing whitespace

--- travis-build.sh
+++ travis-build.sh
@@ -5,7 +5,7 @@ echo "TRAVIS_BRANCH       : $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST : $TRAVIS_PULL_REQUEST"
 echo "Publishing archives for branch $TRAVIS_BRANCH"
 
-./gradlew clean check--stacktrace
+./gradlew clean check --stacktrace
 
 EXIT_STATUS=0
 
